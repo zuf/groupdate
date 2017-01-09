d86920a554daabfea4ad0454999c8dbee50e83b0
Andrew Kane <acekane1@gmail.com>
2017-01-09 01:10:58 -0800

    Working tests

--- test/sqlite_test.rb
+++ test/sqlite_test.rb
@@ -12,4 +12,18 @@ class TestSqlite < Minitest::Test
       true
     end
   end
+
+  def test_where_after
+    skip
+  end
+
+  def call_method(method, field, options)
+    if method == :quarter || options[:time_zone] || options[:day_start] || options[:week_start] || Groupdate.week_start != :sun || (Time.zone && options[:time_zone] != false)
+      error = assert_raises(Groupdate::Error) { super }
+      assert_includes error.message, "not supported for SQLite"
+      skip # after assertions
+    else
+      super
+    end
+  end
 end
